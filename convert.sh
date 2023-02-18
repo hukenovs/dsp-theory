d1ea336ede2bf61ae89e4f110dcc3f2a752d587a
hukenovs <sallador@bk.ru>
2023-02-18 19:34:14 +0300

    Add HTML converter for all lectures

---
+++ convert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get all lectures
+array=()
+find src -name '*.ipynb' -not -path "*.ipynb_checkpoints/*" | sort -V | while read fname;
+do
+  array+=("$fname");
+done
+
+# Merge to single Jupyter Notebook
+ nbmerge "${array[@]}" -o "dsp_theory.ipynb"
+
+# Convert to HTML
+jupyter nbconvert --to html "dsp_theory.ipynb"
